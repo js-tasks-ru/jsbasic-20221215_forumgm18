0dff73a47f912d3b3375d1878f282e72898fecb5
Andrey Shishkin <a.shishkin@holodilnik.ru>
2022-12-21 21:38:39 +0400

    Решена задача 1 из модуля 1. Есть вопрос

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  console.log('typeof n:', typeof n, 'n:', n, 'parseInt(n): ', parseInt(n) );
+
+  if ( typeof n != 'number' || parseInt(n) != n || n < 0 ) return undefined;
+
+  if ( n <= 1  ) return 1;
+  let res = 1;
+  while ( n > 1) {
+    res *= n--;
+    // n--
+  }
+
+  return res;
 }
