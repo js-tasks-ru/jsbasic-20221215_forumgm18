5696e70bf324f4e8a039239b859903990af90acc
Andrey Shishkin <a.shishkin@holodilnik.ru>
2022-12-29 18:47:50 +0400

    Решена задача 3 из модуля 4.

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  let arrHeadCellsValues = Array.prototype.map.call(table.tHead.rows[0].cells, el => el.textContent),
+      indexOfAge = arrHeadCellsValues.indexOf('Age'),
+      indexOfGender = arrHeadCellsValues.indexOf('Gender'),
+      indexOfStatus = arrHeadCellsValues.indexOf('Status');
+
+  for ( row of table.tBodies[0].rows ) {
+    if ( +row.cells[indexOfAge].textContent < 18 ) {
+      row.style.textDecoration = 'line-through'
+    }
+
+    let newClass = row.cells[indexOfGender].textContent === 'm' ? 'male' : 'female';
+    row.classList.add(newClass);
+
+    switch( row.cells[indexOfStatus].dataset.available ) {
+      case 'true':
+        row.classList.add('available');
+        break;
+      case 'false':
+        row.classList.add('unavailable');
+        break;
+      default: 
+        row.setAttribute('hidden', '');
+    }
+
+  };
 }
