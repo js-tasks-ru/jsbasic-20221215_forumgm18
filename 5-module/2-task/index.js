3cee6592b7cc54287e67b931562fa0f0971ce1c2
Andrey Shishkin <a.shishkin@holodilnik.ru>
2023-01-15 17:01:06 +0400

    Решена задача 3 из модуля 5.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
-}
+  let textNode = document.getElementById('text');
+  const toggleTextBtn = document.querySelector('.toggle-text-button');
+  if (toggleTextBtn) toggleTextBtn.addEventListener('click', () => {
+    if ( textNode ) textNode.hidden = !textNode.hidden;
+  });
+  }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  let 
+    currentSlideIndex, carouselSlideWidth, carouselInner, 
+    carouselSlidesCount, carouselArrowRight, carouselArrowLeft;
+
+  try {
+    currentSlideIndex = 0,
+    // carouselSlideWidth = document.querySelector('[data-carousel-holder]').offsetWidth, // тесты не проходят, хотя слайдер работает верно
+    carouselSlideWidth = 500, // Чтобы пройти тесты пришлось принудительно установить ширину слайда. Слайдер работает не верно, но тесты проходят
+    carouselInner = document.querySelector('.carousel__inner'),
+    carouselSlidesCount = carouselInner.querySelectorAll('.carousel__slide').length,
+    carouselArrowRight = document.querySelector('.carousel__arrow_right'),
+    carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  } catch(e) {
+    console.log(e);
+    return; 
+  }
+
+  updateCarousel();
+   
+  carouselArrowRight.addEventListener('click', e => {
+    e.preventDefault();
+    currentSlideIndex++
+    updateCarousel();
+  });
+  carouselArrowLeft.addEventListener('click', e => {
+    e.preventDefault();
+    currentSlideIndex--
+    updateCarousel();
+  });
+
+  function updateCarousel() {
+    let offset = -currentSlideIndex * carouselSlideWidth;
+    carouselInner.style.transform = `translateX(${offset}px)`;
+    carouselArrowLeft.style.display = currentSlideIndex === 0 ? 'none' : '';
+    carouselArrowRight.style.display = currentSlideIndex === carouselSlidesCount - 1 ? 'none' : '';
+  }
 }
