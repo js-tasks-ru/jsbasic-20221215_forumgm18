8b29f5dbec1a634a3cced33cb33a1facf8411576
Andrey Shishkin <a.shishkin@holodilnik.ru>
2023-01-23 10:21:57 +0400

    Решена задача 1 из модуля 8.

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,9 +1,9 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class CartIcon {
+  #initialTopY = null;
   constructor() {
     this.render();
-
     this.addEventListeners();
   }
 
@@ -39,6 +39,25 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (!this.#initialTopY) this.#initialTopY = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    if ( window.pageYOffset > this.#initialTopY && document.documentElement.clientWidth > 767) {
+      let 
+        cartContainerX = document.querySelector('.container').getBoundingClientRect().right + 20,
+        cartWindowX = document.documentElement.clientWidth - this.elem.offsetWidth - 10,
+        cartX = Math.min(cartContainerX, cartWindowX) + 'px';
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1000,
+        right: '10px',
+        left: cartX
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });    }
   }
 }
